b9f11eb4221002962d040422cb602b641feb86ca
Emily Goodrich <emily.goodrich2751@mtec.stu.edu>
2023-10-13 10:12:00 -0600

    added some more styling

--- todoApp.js
+++ todoApp.js
@@ -1,6 +1,8 @@
 
 var createNewButton = document.getElementById('createNewButton');
 var originalPopUp = document.getElementById('peekaboo');
+var createNewTodo = document.getElementById('createNewTodo');
+createNewTodo.className = 'hidden';
 originalPopUp.className = 'hidden';
 function openPopUp() {
     if (originalPopUp.className === 'hidden') {
@@ -54,27 +56,55 @@ let listNameButton = document.getElementById('listNameButton');
 
 
 
-
-
+//var changeTodo = null;
+var currentlyClickedTodo = null;
+var completion = false;
+var todoIdForRemoving = null;
 var currentlyClickedList = null;
 var todoList = [];
 function onListClick(id) {
     currentlyClickedList = lists.find(l => l.id === id);
     todoList = currentlyClickedList.todoList
+    if (todoList) {
+    currentlyClickedTodo = todoList.find(l => l.id === id);
+    todoIdForRemoving = currentlyClickedTodo.id;
+    completion = currentlyClickedTodo.completed;
+    changeTodo = document.getElementById(`changeTodo${currentlyClickedTodo.id}`)
+    }
     render();
 }
 
+
+
+
+function completedTodo() {
+    completion = true;
+    if (completion === true) {
+        console.log('its tru')
+        changeTodo.className = ''
+        
+    }
+    render();
+}
+
+function deleteTodo() {
+    
+    let eliminated = todoList.filter(x => {
+        return x.id != todoIdForRemoving;
+    })
+    console.log(eliminated)
+    console.log(todoIdForRemoving)
+}
 function render() {
     
     let listsHtml = '<ul class="list-group">';
     lists.forEach((list) => {
-        listsHtml += `<li class="listName" id="theCurrentList${list.id}"><a href="#" onclick="onListClick(${list.id})">${list.name}</a></li>`;
+        listsHtml += `<li class="listName" id="theCurrentList${list.id}"><a href="#" class="normalListColor" onclick="onListClick(${list.id})">${list.name}</a></li>`;
         
     
     });
     listsHtml += '</ul>';
-    console.log(listsHtml)
-    console.log(lists)
+    
     
     document.getElementById('listOfLists').innerHTML = listsHtml;
 
@@ -82,18 +112,23 @@ function render() {
     if (currentlyClickedList) {
     let currentListName = currentlyClickedList.name;
     document.getElementById('listTitle').innerHTML = currentListName;
-    console.log(currentlyClickedList.todoList)
+        document.getElementById(`theCurrentList${currentlyClickedList.id}`).className = 'chosenListClass'
+    if (currentlyClickedTodo) {
+    
+    
+    }
+    createNewTodo.className = 'createNewTodoClass';
     }
     
 
     let todosHtml = '<ul class="list-group-flush">';
     todoList.forEach((todo) => {
-        todosHtml += `<li class="list-group-item">${todo.name}</li>`;
+        todosHtml += `<li class="list-group-item"><span class="theTodoClass"><span><span><button onclick="completedTodo(${todo.id})" class="todoCompleteButton"></button></span><span class="todoWords" id="changeTodo${todo.id}">${todo.name}</span></span><span class="deleteTodoButton" onclick="deleteTodo()">X</span></span></li>`;
     });
 
     document.getElementById('theTodoList').innerHTML = todosHtml;
-}
 
+}
 var listNameInput = document.getElementById('listNameInput');
 
 var lists = [];
@@ -123,13 +158,16 @@ todoNameButton.addEventListener('click', function addTodo() {
     const todoText = todoNameInput.value;
     
     todoNameInput.value = "";
-    
+    var ticks = Date.now();
     if (todoText) {
         todoList.push({
+            id: ticks,
             name: todoText,
             completed: false
         })
         render();
+        
+        popUpNewTodo.className = 'hidden';
     }
 
 })
