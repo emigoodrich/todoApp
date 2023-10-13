6cd688e2ca5c5c5017be4dd122f13175014e9a97
Emily Goodrich <emily.goodrich2751@mtec.stu.edu>
2023-10-13 07:32:01 -0600

    i made lists and then made todolists for each list, it works

--- todoApp.js
+++ todoApp.js
@@ -1,5 +1,4 @@
 
-var numberOfLists = 0; //nvm 
 var createNewButton = document.getElementById('createNewButton');
 var originalPopUp = document.getElementById('peekaboo');
 originalPopUp.className = 'hidden';
@@ -54,91 +53,52 @@ function closeXButtonTodo() {
 let listNameButton = document.getElementById('listNameButton');
 
 
-var todoNameInput = document.getElementById('todoNameInput');
 
 
 
-const currentList = {
-    1: {
-        name: "shopping list",
-        todos: [
-            {
-                text: 'bananas',
-                completed: false
-            }
-        ]
-    }
+var currentlyClickedList = null;
+var todoList = [];
+function onListClick(id) {
+    currentlyClickedList = lists.find(l => l.id === id);
+    todoList = currentlyClickedList.todoList
+    render();
 }
-const listOfLists = {
-    1: { name: 'First lisjt' },
-    2: { name: 'what ill do for college' },
-    3: { name: 'history of pizza' }
-}
-
-const groupOfLists = {
-    1: {
-        name: 'first list',
-        todos: [
-            {
-                text: 'cats',
-                completed: false
-            },
-            {
-                text: 'phone',
-                complete: false
-            }
-        ]
-    }
-}
-//const currentList = groupOfLists[0];
 
 function render() {
-
+    
     let listsHtml = '<ul class="list-group">';
     lists.forEach((list) => {
-        listsHtml += `<li class="listName" id="theCurrentList"><div id='list${list.id}>${list.name}</div></li>`;
+        listsHtml += `<li class="listName" id="theCurrentList${list.id}"><a href="#" onclick="onListClick(${list.id})">${list.name}</a></li>`;
+        
     
-     // listsHtml += `<input type="button" onclick="() => lists.remove(${list.id})">X</input>`
     });
     listsHtml += '</ul>';
-
+    console.log(listsHtml)
     console.log(lists)
-
-    // print out the lists
+    
     document.getElementById('listOfLists').innerHTML = listsHtml;
 
-    // print out the name of the current list
-    
-    //let currentListName = document.getElementById('theCurrentList${list.id}');
-    //document.getElementById('listTitle').innerHTML = currentListName;
-    
-    //put everything on the right or in main right here
 
-    // iterate over the todos in the current list
+    if (currentlyClickedList) {
+    let currentListName = currentlyClickedList.name;
+    document.getElementById('listTitle').innerHTML = currentListName;
+    console.log(currentlyClickedList.todoList)
+    }
+    
+
     let todosHtml = '<ul class="list-group-flush">';
     todoList.forEach((todo) => {
-        todosHtml += `<li class="list-group-item">${todo.text}</li>`;
+        todosHtml += `<li class="list-group-item">${todo.name}</li>`;
     });
-    // for (var i = 0; i < lists.length; i++) {
-    //     console.log(document.getElementById(`list${lists[i].id}`));
-    //     var currentListDiv = document.getElementById(`list${lists[i].id}`);
-    //          currentListDiv.addEventListener("click", function() {
-    //             console.log("you clicked region number " + index);
-    //           });
-    //  }
-    
-    // print out the todos
-   // document.getElementById('current-list-todos').innerHTML = todosHtml;
+
+    document.getElementById('theTodoList').innerHTML = todosHtml;
 }
 
 var listNameInput = document.getElementById('listNameInput');
 
 var lists = [];
-var todoList = [];
+
 listNameButton.addEventListener('click', function creatingList() {
-    // listTitle.textContent = listNameInput.value;
-    // const newListNameElement = document.createElement('div');
-    // newListNameElement.classList.add('listOfTodoLists');
     
     const text = listNameInput.value;
     
@@ -149,54 +109,27 @@ listNameButton.addEventListener('click', function creatingList() {
             id: ticks,
             name: text,
             completed: false,
-            array: [{
-                id: ticks,
-                name: todoList
-        }]
+            todoList: []
         })
         
         render();
+        originalPopUp.className = 'hidden';
     }
-    document.getElementById('mainItem');
 })
-
+var todoNameInput = document.getElementById('todoNameInput');
 var todoNameButton = document.getElementById('todoNameButton');
 todoNameButton.addEventListener('click', function addTodo() {
 
-    const text = todoNameInput.value;
+    const todoText = todoNameInput.value;
     
     todoNameInput.value = "";
     
-    if (text) {
+    if (todoText) {
         todoList.push({
-            name: text,
+            name: todoText,
             completed: false
         })
-        // function printingOutTodos() {
-        //     document.getElementById('theTodoList') = document.createElement('li')//not it but close more or less
-        //     const textnode = document.createTextNode(`${text}`)
-        //     document.getElementById('theTodoList').append(textnode)
-        //     // document.getElementById('theTodoList').append(`<li id="listItemBox">`);
-        //     // document.getElementById('theTodoList').append(`${text}`);
-        //     // document.getElementById('theTodoList').append(`</li>`);
-        // }
-        printingOutTodos();
+        render();
     }
-console.log(todoList)
+
 })
-
-
-
-// for (let i = 0; i < lists.length; i++) {
-//     console.log(lists[i].name)
-//     lists[i].addEventListener('click', function chooseMe() {
-//         alert('yessir')
-//         console.log(lists[i].name)
-//     })
-//  }
-for (let i = 0; i < lists.length; i++) (function(i){
-    lists[i].onclick = function() {
-        console.log("yay")
-        console.log("You clicked:" + lists[i].name)
-    }
-})(i);
